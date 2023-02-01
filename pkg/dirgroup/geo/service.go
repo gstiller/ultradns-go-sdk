8d0c1033e72b1424d0752528bded88c8c06906cd
Gyorgy Stiller <gyorgy.stiller@sony.com>
2023-02-01 23:30:51 +0100

    First geoip service

--- pkg/dirgroup/geo/service.go
+++ pkg/dirgroup/geo/service.go
@@ -3,13 +3,16 @@ package geo
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/ultradns/ultradns-go-sdk/pkg/client"
 	"github.com/ultradns/ultradns-go-sdk/pkg/errors"
+	"github.com/ultradns/ultradns-go-sdk/pkg/helper"
 )
 
 const (
 	serviceName = "DirGroupGeoIPGroup"
+	basePath    = "accounts/"
 	// https://api.ultradns.com/accounts/{accountName}/dirgroups/geo/
 )
 
@@ -34,23 +37,118 @@ func Get(c *client.Client) (*Service, error) {
 	return &Service{c}, nil
 }
 
+func (s *Service) CreateDirGroupGeoIP(dirGroupGeoIP *DirGroupGeoIP, cfn *client.Config) (*http.Response, error) {
+	target := client.Target(&client.SuccessResponse{})
+
+	if s.c == nil {
+		return nil, errors.ServiceError(serviceName)
+	}
+
+	createPath := basePath + cfn.Username + "/dirgroups/geo/" + dirGroupGeoIP.Name
+	res, err := s.c.Do(http.MethodPost, createPath, dirGroupGeoIP, target)
+
+	if err != nil {
+		geoIPGroupName := ""
+		geoIPGroupName = dirGroupGeoIP.Name
+
+		return nil, errors.CreateError(serviceName, geoIPGroupName, err)
+	}
+
+	return res, nil
+}
+
 func (s *Service) dirGroupURI(groupName string) string {
 	return fmt.Sprintf("accounts/%s/dirgroups/geo/%s", "testing", groupName)
 }
 
-func (s *Service) Read(groupName string) (*http.Response, *DirGroupGeoIP, error) {
-	target := client.Target(&DirGroupGeoIP{})
+func (s *Service) ReadDirGroupGeoIP(dirGroupGeoIPName string, cfn *client.Config) (*http.Response, *Response, error) {
+	target := client.Target(&Response{})
+	dirGroupGeoIPName = url.PathEscape(dirGroupGeoIPName)
 
 	if s.c == nil {
 		return nil, nil, errors.ServiceError(serviceName)
 	}
 
-	res, err := s.c.Do(http.MethodGet, s.dirGroupURI(groupName), nil, target)
+	readPath := basePath + cfn.Username + "/dirgroups/geo/" + dirGroupGeoIPName
+	res, err := s.c.Do(http.MethodGet, readPath, nil, target)
 	if err != nil {
-		return nil, nil, errors.ReadError(serviceName, groupName, err)
+		return nil, nil, errors.ReadError(serviceName, dirGroupGeoIPName, err)
 	}
 
-	groupRes := target.Data.(*DirGroupGeoIP)
+	dirGroupGeoIPResponse := target.Data.(*Response)
 
-	return res, groupRes, nil
+	return res, dirGroupGeoIPResponse, nil
+}
+
+func (s *Service) UpdateDirGroupGeoIP(dirGroupGeoIPName string, dirGroupGeoIP *DirGroupGeoIP, cfn *client.Config) (*http.Response, error) {
+	target := client.Target(&client.SuccessResponse{})
+	dirGroupGeoIPName = url.PathEscape(dirGroupGeoIPName)
+
+	if s.c == nil {
+		return nil, errors.ServiceError(serviceName)
+	}
+
+	updatePath := basePath + cfn.Username + "/dirgroups/geo/" + dirGroupGeoIPName
+	res, err := s.c.Do(http.MethodPut, updatePath, dirGroupGeoIP, target)
+
+	if err != nil {
+		return nil, errors.UpdateError(serviceName, dirGroupGeoIPName, err)
+	}
+
+	return res, nil
+}
+
+func (s *Service) PartialUpdateDirGroupGeoIP(dirGroupGeoIPName string, dirGroupGeoIP *DirGroupGeoIP, cfn *client.Config) (*http.Response, error) {
+	target := client.Target(&client.SuccessResponse{})
+	dirGroupGeoIPName = url.PathEscape(dirGroupGeoIPName)
+
+	if s.c == nil {
+		return nil, errors.ServiceError(serviceName)
+	}
+
+	partiallyUpdatePath := basePath + cfn.Username + "/dirgroups/geo/" + dirGroupGeoIPName
+	res, err := s.c.Do(http.MethodPatch, partiallyUpdatePath, dirGroupGeoIP, target)
+
+	if err != nil {
+		return nil, errors.PartialUpdateError(serviceName, dirGroupGeoIPName, err)
+	}
+
+	return res, nil
+}
+
+func (s *Service) DeleteDirGroupGeoIP(dirGroupGeoIPName string, cfn *client.Config) (*http.Response, error) {
+	target := client.Target(&client.SuccessResponse{})
+	dirGroupGeoIPName = url.PathEscape(dirGroupGeoIPName)
+
+	if s.c == nil {
+		return nil, errors.ServiceError(serviceName)
+	}
+
+	deletePath := basePath + cfn.Username + "/dirgroups/geo/" + dirGroupGeoIPName
+	res, err := s.c.Do(http.MethodDelete, deletePath, nil, target)
+
+	if err != nil {
+		return nil, errors.DeleteError(serviceName, dirGroupGeoIPName, err)
+	}
+
+	return res, nil
+}
+
+func (s *Service) ListDirGroupGeoIP(queryInfo *helper.QueryInfo, cfn client.Config) (*http.Response, *ResponseList, error) {
+	target := client.Target(&ResponseList{})
+
+	if s.c == nil {
+		return nil, nil, errors.ServiceError(serviceName)
+	}
+
+	listPath := basePath + cfn.Username + "/dirgroups/geo"
+	res, err := s.c.Do(http.MethodGet, listPath+queryInfo.URI(), nil, target)
+
+	if err != nil {
+		return nil, nil, errors.ListError(serviceName, listPath+queryInfo.URI(), err)
+	}
+
+	dirGroupGeoIPListResponse := target.Data.(*ResponseList)
+
+	return res, dirGroupGeoIPListResponse, nil
 }
--- pkg/dirgroup/geo/structures.go
+++ pkg/dirgroup/geo/structures.go
@@ -1,7 +1,28 @@
 package geo
 
+import "github.com/ultradns/ultradns-go-sdk/pkg/helper"
+
+// Account Level Group is defined as:
+// {
+//	 "name": "accountGeoGroup",
+//	 "description": "A sample group",
+//	 "codes": ["Z6", "RU"]
+// }
+
 type DirGroupGeoIP struct {
 	Name        string   `json:"name"`
 	Description string   `json:"description"`
 	Codes       []string `json:"codes"`
+}
+
+type Response struct {
+	Name        string   `json:"name,omitempty"`
+	Description string   `json:"description,omitempty"`
+	Codes       []string `json:"codes,omitempty"`
+}
+
+type ResponseList struct {
+	QueryInfo      *helper.QueryInfo  `json:"queryInfo,omitempty"`
+	CursorInfo     *helper.CursorInfo `json:"cursorInfo,omitempty"`
+	DirGroupGeoIPs []*Response        `json:"zones,omitempty"`
 }
